a0a7266323029b0efe63d26b4eb693aef853a8bc
joe.zhao <zjiuzhou@gmail.com>
2020-12-20 16:30:47 +0800

    修改readme

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# spo_extract
-基于transformers的三元组抽取
+# 基于transformers框架的三元组抽取baseline
+
+## how to use
+
++ 下载预训练模型，放到bert目录下，下载训练数据放到data目录下
++ 安装`transformers`,`pip install transformers`
++ 执行train.py文件
+
+预训练模型下载地址
+
++ bert
+https://huggingface.co/bert-base-chinese/tree/main
+
++ roberta
+https://huggingface.co/hfl/chinese-roberta-wwm-ext/tree/main
+
+
+训练数据下载地址
+
+链接：https://pan.baidu.com/s/1rNfJ88OD40r26RR0Lg6Geg 
+提取码：a9ph 
+
+## model structure
+
--- model.py
+++ model.py
@@ -3,31 +3,6 @@ import torch.nn as nn
 import torch
 
 
-class GraphModel(BertPreTrainedModel):
-    def __init__(self, config):
-        super().__init__(config)
-        self.bert = BertModel(config)
-        # self.layer_norm = nn.LayerNorm([16, 768])
-        self.dense = nn.Linear(config.hidden_size, 2)
-        # self.layer_norm = nn.LayerNorm()
-        self.dense_subject_position = nn.Linear(2, config.hidden_size)
-        self.dense_object = nn.Linear(config.hidden_size, 2)
-
-    def forward(self,
-                input_ids,
-                subject_position,
-                attention_mask=None):
-        output = self.bert(input_ids, attention_mask=attention_mask)
-        # output = self.layer_norm(output)
-        output = self.dense(output[0])
-        subject_out = torch.sigmoid(output)
-
-        subject_position = self.dense_subject_position(subject_position)
-        object_out = subject_out + subject_position
-
-        return subject_out, object_out
-
-
 class SubjectModel(BertPreTrainedModel):
     def __init__(self, config):
         super().__init__(config)
